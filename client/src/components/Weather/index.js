987396f3c43b6980b2679a8c3d95fd2cbd52abf3
kylepadilla <kylepadilla1@gmail.com>
2019-05-08 01:04:34 -0400

    having trouble with ternary operator in dashboard component

--- client/src/components/Weather/index.js
+++ client/src/components/Weather/index.js
@@ -2,15 +2,18 @@ import React, {Component} from 'react'
 import { OpenWeatherMap } from 'react-weather';
 import axios from 'axios'
 
-class Weather extends Component {
-state = {
+class WeatherContainer extends Component {
+    constructor () {
+        super()
+this.state = {
     lon: '',
     lat: '',
     city: '',
     country:''
 }
+}
 
-Geocode(){
+componentDidMount(){
 let api = 'AIzaSyDcjOBGMGmfQdfQ5odnm2azxH11GQ_YMZo'
     axios.get("https://www.googleapis.com/geolocation/v1/geolocate?key=" + api).then(res=>{
         console.log(res.data)
@@ -19,22 +22,23 @@ let api = 'AIzaSyDcjOBGMGmfQdfQ5odnm2azxH11GQ_YMZo'
             lat: res.data.lat
         })
         }).then(
-            axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng="+this.state.lat+","+this.state.lon+"&key="+api).then(res=>{
+            axios.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + this.state.lat + "," + this.state.lon + "&key=" + api).then(res=>{
                 console.log(res.data)
                 this.setState({
                     city: res.data.city,
                     country: res.data.country
                 })
             })
-        )
-}
+        )}
+
 
 render(){
 return(
 <div className="weather">
 <OpenWeatherMap city={this.state.city} country={this.state.country} appid="35784240997be91f92a57a40329788d1" />
 </div>
-)}
-
+)
 }
-export default Weather;
+}
+export default WeatherContainer
+
