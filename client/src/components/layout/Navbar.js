9eebd1311b03c70a233dea3a3cb22aef291eda09
kylepadilla <kylepadilla1@gmail.com>
2019-05-09 14:35:39 -0400

    nav

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import logo from '../../assets/images/Vendi-README_image.png'
 
 
 
@@ -14,20 +14,13 @@ class Navbar extends Component {
       <div className="navbar-fixed">
         <nav className="z-depth-0">
           <div className="nav-wrapper white">
-            <Link
-              to="/"
-              style={{
-                fontFamily: "monospace"
-              }}
-              className="col s5 brand-logo center black-text"
-            >
-              <i className="material-icons">code</i>
-              VENDEMON
+            <Link to="/" style={{fontFamily: "monospace"}}className="col s5 brand-logo left-align black-text">
+                  <img src={logo} alt="unavailable"/>
             </Link>
-           
+
           </div>
         </nav>
-      </div>
+      </div> 
     );
   }
 }
